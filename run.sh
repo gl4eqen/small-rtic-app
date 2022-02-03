666357bd69ae4819e505f51294717c0b02992e1f
Gabriel Górski <gabriel.gorski@grepit.se>
2022-02-04 00:27:40 +0100

    Add source to objdump output + optimisation flags

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
-rustc +nightly --target thumbv7em-none-eabi --emit=obj -Z emit-stack-sizes big-functions.rs && \
+rustc +nightly -g -C opt-level=0 --target thumbv7em-none-eabi --emit=obj -Z emit-stack-sizes big-functions.rs && \
+arm-none-eabi-objdump -S -C -d big-functions.o && \
+echo && echo && \
 stack-sizes big-functions.o
